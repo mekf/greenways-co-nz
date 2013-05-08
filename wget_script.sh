dcea276c5dbbf737ddbb101d0148d17119246480
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-08 17:15:15 +1200

    new scrape

--- wget_script.sh
+++ wget_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wget --recursive --no-clobber --limit-rate=30K --page-requisites --html-extension --domains greenways.co.nz --no-parent
 wget --recursive --convert-links --no-clobber --limit-rate=30K --page-requisites --html-extension --domains greenways.co.nz --no-parent
 
  # --convert-links \
