81b6d0d23bfaa1cab13659b52c6228756273279d
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-08 15:13:53 +1200

    scraped

---
+++ wget_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget --recursive --convert-links --no-clobber --limit-rate=30K --page-requisites --html-extension --domains greenways.co.nz --no-parent
+
+ # --convert-links \
+ # --restrict-file-names=windows \
