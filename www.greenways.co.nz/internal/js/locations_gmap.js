25a83ef8d2fbb0bf29f9ba172c1ce968496f212c
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-16 05:05:52 +1200

    working json

--- www.greenways.co.nz/internal/js/locations_gmap.js
+++ www.greenways.co.nz/internal/js/locations_gmap.js
@@ -1,22 +1,5 @@
-var map;
-var geocoder;
-
-// nelson = center of NZ, tweaked to the left
-var nzCenterLatLng = new google.maps.LatLng(-40.86368,172.441406);
-var wlgLatLng = new google.maps.LatLng(-41.28646, 174.776236);
-
-var mapOptions = {
-  center: nzCenterLatLng,
-  zoom: 6,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-function initialize () {
-  var map = new google.maps.Map(
-    document.getElementById('map-canvas'), mapOptions
-    );
-  }
-var locations = (function() {
+// Load locations.json
+var locationsJSON = (function() {
   var json = null;
   $.ajax({
     'async': false,
@@ -30,21 +13,60 @@ var locations = (function() {
   return json
 })();
 
-function createSidebarEntryNew(name, address, phone, region) {
-    var div = document.createElement('div');
-    var html = '<div style="margin-bottom: 20px;" class="' + region + ' region"><b>' + name + '</b><br/>' + address + '<br/>' + region + '<br/>' + phone + '</div>';
-    div.innerHTML = html;
-    div.id = 'block';
-    div.style.cursor = 'pointer';
-    div.style.fontSize = '18px'; 
-    return div;
-  }
+function initialize () {
+  loadMap();
+  loadSidebar();
+}
 
-for (var i = 0; i < markers.length; i++) {
-  var name = markers[i].getAttribute('name');
-  var address = markers[i].getAttribute('address');
-  var phone = markers[i].getAttribute('phone');
-  var region = markers[i].getAttribute('region');
-  var sidebarEntry = createSidebarEntryNew(name, address, phone, region);
-  sidebar.appendChild(sidebarEntry);
+function loadMap() {
+  var nzCenterLatLng = new google.maps.LatLng(-40.86368,172.441406); // left nelson = center of NZ,
+  var wlgLatLng = new google.maps.LatLng(-41.28646, 174.776236);
+
+  var mapOptions = {
+    center: nzCenterLatLng,
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(
+    document.getElementById('map-canvas'), mapOptions
+    );
+}
+
+function loadSidebar() {
+  var address = document.getElementById('addressInput').value;
+  var geocoder = new google.maps.Geocoder();
+  
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      createSidebar();
+    } else {
+      alert(address + ' not found');
+    }
+  });
+}
+
+function createSidebar() {
+  var sidebar = document.getElementById('sidebar');
+  var locEntry = locationsJSON.entries
+
+  for (var i = 0; i < locEntry.length; i++) {
+    var name =    locEntry[i].name;
+    var address = locEntry[i].address;
+    var phone =   locEntry[i].phone;
+    var region =  locEntry[i].region;
+    var sidebarEntry = createSidebarEntryNew(name, address, phone, region);
+    sidebar.appendChild(sidebarEntry);
+  }
+  return sidebar;
+}
+
+function createSidebarEntryNew(name, address, phone, region) {
+  var div = document.createElement('div');
+  var html = '<div style="margin-bottom: 20px;" class="' + region + ' region"><b>' + name + '</b><br/>' + address + '<br/>' + region + '<br/>' + phone + '</div>';
+  div.innerHTML = html;
+  div.id = 'block';
+  div.style.cursor = 'pointer';
+  div.style.fontSize = '18px'; 
+  return div;
 }
