a67e02bc81241df46950ad51939d4e1353b5877e
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-17 01:40:11 +1200

    nearly finished gmap

--- www.greenways.co.nz/internal/js/locations_gmap.js
+++ www.greenways.co.nz/internal/js/locations_gmap.js
@@ -13,15 +13,14 @@ var locationsJSON = (function() {
   return json;
 })();
 
+var locEntries = locationsJSON.entries;
 var map;
 var geocoder;
-var locEntry = locationsJSON.entries;
+var markersArray = [];
 
 function locationsInitialize() {
   loadMap();
-  buildSidebar();
-  // loadSidebar();
-  console.log(locationsJSON);
+  loadSidebar();
 }
 
 function loadMap() {
@@ -37,58 +36,73 @@ function loadMap() {
 }
 
 function loadSidebar() {
-  var address = document.getElementById('addressInput').value;
-  // locEntry.length
-  for (var i = 0; i < 10; i++) {
-    var searchAddress = locEntry[i].address + ', ' + locEntry[i].region + ', ' + 'New Zealand';
-    geocodeSearch(searchAddress);
-  }
-}
-
-function geocodeSearch(string) {
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ 'address': string }, function (geoResults, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      searchLocations(geoResults);
-    } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-      alert('overload');
-    }
-    else {
-      alert(string + ' not found');
-    }
-  });
-}
-
-function searchLocations(geoInfo) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: geoInfo[0].geometry.location
-  });
-  // map.setCenter(results[0].geometry.location);
-  // buildSidebar();
-}
-
-function buildSidebar() {
   var sidebar = document.getElementById('sidebar');
 
-  for (var i = 0; i < locEntry.length; i++) {
-    var name    = locEntry[i].name;
-    var address = locEntry[i].address;
-    var phone   = locEntry[i].phone;
-    var region  = locEntry[i].region;
+  for (var i = 0; i < locEntries.length; i++) {
+    var name    = locEntries[i].name;
+    var address = locEntries[i].address;
+    var phone   = locEntries[i].phone;
+    var region  = locEntries[i].region;
     var sidebarEntry = createSidebarEntry(name, address, phone, region);
     sidebar.appendChild(sidebarEntry);
+    // need on_click event (center + info window)
   }
   return sidebar;
 }
 
-function createMarker(point, name, address, phone, region) {
-  var marker = new GMarker(point);
-  var html = '<b>' + name + '</b> <br/>' + address + '<br/>' + region + '<br/>' + phone;
-  GEvent.addListener(marker, 'click', function() {
-    marker.openInfoWindowHtml(html);
+// might be redundant
+// somewhat similar to the searchLocations of their old code
+function searchLocations() {
+  var address = document.getElementById('addressInput').value;
+  // locEntries.length \ will overload the querry. need timeout
+  for (var i = 0; i < 10; i++) {
+    var searchAdd = locEntries[i].address + ', ' + locEntries[i].region + ', ' + 'New Zealand';
+    geocodeSearch(searchAdd);
+  }
+}
+
+
+function regionChange(region) {
+  deleteMarkersOverlays();
+
+  var searchAdd = [];
+  for (var i = 0; i < locEntries.length; i++) {
+    if (locEntries[i].region == region) {
+      searchAdd.push(locEntries[i].address + ', ' + locEntries[i].region + ', ' + 'New Zealand');
+    }
+  }
+
+  // Auckland will easily overload the geocoding request
+  if (region != 'Auckland') {
+    for (var j = 0; j < searchAdd.length; j++) {
+      geocodeSearch(searchAdd[j]);
+    }
+  }
+}
+
+function geocodeSearch(addString) {
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ 'address': addString }, function (geoResults, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      plotMarkersLocations(geoResults);
+    } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+      alert('overload');
+    }
+    else {
+      alert(addString + ' not found');
+    }
   });
-  return marker;
+}
+
+function plotMarkersLocations(geoInfo) {
+  var markerOptions = {
+    map: map,
+    position: geoInfo[0].geometry.location
+  };
+  var marker = new google.maps.Marker(markerOptions);
+  markersArray.push(marker);
+  // map.setCenter(markerOptions.position);
+  // need on_click event for the marker (center + info window)
 }
 
 function createSidebarEntry(name, address, phone, region) {
@@ -99,4 +113,23 @@ function createSidebarEntry(name, address, phone, region) {
   div.style.cursor = 'pointer';
   div.style.fontSize = '18px';
   return div;
+}
+
+function deleteMarkersOverlays() {
+  if (markersArray) {
+    for (var i in markersArray) {
+      markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
+  }
+}
+
+// Not yet ported
+function createMarker(point, name, address, phone, region) {
+  var marker = new GMarker(point);
+  var html = '<b>' + name + '</b> <br/>' + address + '<br/>' + region + '<br/>' + phone;
+  GEvent.addListener(marker, 'click', function() {
+    marker.openInfoWindowHtml(html);
+  });
+  return marker;
 }
