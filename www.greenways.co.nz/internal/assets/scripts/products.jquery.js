5c271b290258889ac74a3eb7a691452815610cc6
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-24 04:33:23 +1200

    monkey patch js, apple.png from wayback machine

--- www.greenways.co.nz/internal/assets/scripts/products.jquery.js
+++ www.greenways.co.nz/internal/assets/scripts/products.jquery.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-    $('.hide').hide();
+  $('.hide').hide();
 
-    $('#p-1309911661').click(function() {
-      $('#d-1309911635').hide();
-      $('#d-1309911661').fadeIn('slow');
-    });
+  // Monkey Patch from what available from scraped data
+
+  $('#d-1309911635').fadeIn('slow');
+
+  $('#p-1309911635').click(function() {
+    $('.hide').hide();
+    $('#d-1309911635').fadeIn('slow');
+  });
 });
 
+// Rationale | Not working yet
+
 // get the current id then hide the list
-
 // get the clicked-on id then show the list
-
 // set the clicked-on id as new current
+
+// $currentLink = $('a.current');
+// $currentImgID = $currentLink.children().attr('id');
+// // p-1309911635
+// $currentUnHideID = $currentImgID.replace(/p/g, 'd');
+// // d-1309911635
+// // $currentUnHideID.show();
--- www.greenways.co.nz/internal/assets/scripts/products.jquery.legacy.js
+++ www.greenways.co.nz/internal/assets/scripts/products.jquery.legacy.js
@@ -3,6 +3,62 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 
 jQuery(document).ready(function() {
+
+  // Monkey Patch from what available from scraped data
+  // hide all list-wrap | show the first one
+  jQuery('.hide').hide();
+  jQuery('#d-1309911635').fadeIn('slow');
+
+  // l1
+  jQuery('#p-1309911635').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1309911635').fadeIn('slow');
+  });
+  jQuery('#p-1309911661').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1309911661').fadeIn('slow');
+  });
+  jQuery('#p-1309911691').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1309911691').fadeIn('slow');
+  });
+  jQuery('#p-1309911709').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1309911709').fadeIn('slow');
+  });
+  jQuery('#p-1309911730').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1309911730').fadeIn('slow');
+  });
+
+  // l2
+  jQuery('#p-1310374005').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1310374005').fadeIn('slow');
+  });
+  jQuery('#p-1310380353').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1310380353').fadeIn('slow');
+  });
+  jQuery('#p-1310380375').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1310380375').fadeIn('slow');
+  });
+  jQuery('#p-1310380416').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1310380416').fadeIn('slow');
+  });
+  jQuery('#p-1310380461').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1310380461').fadeIn('slow');
+  });
+  
+  // l3
+  jQuery('#p-1331109811').click(function() {
+    jQuery('.hide').hide();
+    jQuery('#d-1331109811').fadeIn('slow');
+  });
+
   jQuery(".focus").hover(function() {
     jQuery(this).siblings().stop(true, true).fadeTo('400', 0.5, function() {      
       jQuery(".focus").hover(function() {
@@ -25,7 +81,7 @@ window.addEvent('domready', function(){
     duration: 800,
     transition: Fx.Transitions.Back.easeInOut,
     wait: true
-});
+  });
    
   //-------------------------------------
   // EVENTS for the button "previous"
